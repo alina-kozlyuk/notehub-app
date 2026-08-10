3e2ae14bdc93c0a5ff31e9a6a43291ef491a34d5
Alina Kozlyuk <talina787@icloud.com>
2026-08-10 13:18:44 +0300

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,15 @@
 // next.config.ts
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "ac.goit.global",
+        hostname: 'ac.goit.global',
       },
     ],
   },
   reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        // Замінити URL нижче на реальну адресу бекенду, яку вам надали в GoIT (наприклад, https://api.goit.global або іншу)
-        destination: 'https://api.goit.global/:path*', 
-      },
-    ];
-  },
 };
 
 export default nextConfig;
